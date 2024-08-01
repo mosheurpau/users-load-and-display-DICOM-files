e25cf1134c9d0eb83150a7993d856cf3471230de
mosheurpau <mosheur.pau@gamil.com>
2024-08-01 12:53:38 +0600

    Added functionalities for zooming, rotating, inverting, and resetting the viewport

--- src/CornerstoneViewer.jsx
+++ src/CornerstoneViewer.jsx
@@ -15,28 +15,114 @@ function DicomViewer() {
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
-    const imageId = cornerstoneWADOImageLoader.wadouri.fileManager.add(file);
+    if (file) {
+      const imageId = cornerstoneWADOImageLoader.wadouri.fileManager.add(file);
 
-    cornerstone.loadImage(imageId).then((image) => {
-      cornerstone.displayImage(dicomImageElement.current, image);
-    });
+      cornerstone.loadImage(imageId).then((image) => {
+        cornerstone.displayImage(dicomImageElement.current, image);
+      });
+    }
+  };
+
+  const handleZoomIn = () => {
+    const viewport = cornerstone.getViewport(dicomImageElement.current);
+    viewport.scale += 0.1;
+    cornerstone.setViewport(dicomImageElement.current, viewport);
+  };
+
+  const handleZoomOut = () => {
+    const viewport = cornerstone.getViewport(dicomImageElement.current);
+    viewport.scale -= 0.1;
+    cornerstone.setViewport(dicomImageElement.current, viewport);
+  };
+
+  const handleRandomZoom = () => {
+    const viewport = cornerstone.getViewport(dicomImageElement.current);
+    viewport.scale = Math.random() * 2;
+    cornerstone.setViewport(dicomImageElement.current, viewport);
+  };
+
+  const handleRotateDelta = () => {
+    const viewport = cornerstone.getViewport(dicomImageElement.current);
+    viewport.rotation += 30;
+    cornerstone.setViewport(dicomImageElement.current, viewport);
+  };
+
+  const handleInvert = () => {
+    const viewport = cornerstone.getViewport(dicomImageElement.current);
+    viewport.invert = !viewport.invert;
+    cornerstone.setViewport(dicomImageElement.current, viewport);
+  };
+
+  const handleResetViewport = () => {
+    const image = cornerstone.getImage(dicomImageElement.current);
+    const defaultViewport = cornerstone.getDefaultViewportForImage(
+      dicomImageElement.current,
+      image
+    );
+    cornerstone.setViewport(dicomImageElement.current, defaultViewport);
   };
 
   return (
-    <div>
-      <div className="my-5">
-        <input
-          className="file-input file-input-sm file-input-bordered file-input-success w-full max-w-xs"
-          type="file"
-          accept=".dcm"
-          onChange={handleFileChange}
-        />
+    <div className="  text-center">
+      <input
+        className="file-input file-input-bordered file-input-success w-full max-w-xs file-input-sm my-2"
+        type="file"
+        accept=".dcm"
+        onChange={handleFileChange}
+      />{" "}
+      <div className="bg-slate-800">
+        <div
+          ref={dicomImageElement}
+          style={{ width: "512px", height: "512px", background: "black" }}
+        ></div>
+        <div className="mt-5 text-center ">
+          <button
+            className="btn btn-outline btn-success btn-sm"
+            onClick={handleZoomIn}
+          >
+            Zoom In
+          </button>
+          <button
+            className="btn btn-outline btn-success btn-sm"
+            onClick={handleZoomOut}
+            style={{ marginLeft: "10px" }}
+          >
+            Zoom Out
+          </button>
+          <button
+            className="btn btn-outline btn-success btn-sm"
+            onClick={handleRandomZoom}
+            style={{ marginLeft: "10px" }}
+          >
+            Random Zoom
+          </button>
+        </div>
+
+        <div className="mt-2 text-center">
+          <button
+            className="btn btn-outline btn-success btn-sm mb-5"
+            onClick={handleRotateDelta}
+            style={{ marginLeft: "10px" }}
+          >
+            Rotate Delta 30
+          </button>
+          <button
+            className="btn btn-outline btn-success btn-sm"
+            onClick={handleInvert}
+            style={{ marginLeft: "10px" }}
+          >
+            Invert
+          </button>
+          <button
+            className="btn btn-outline btn-success btn-sm"
+            onClick={handleResetViewport}
+            style={{ marginLeft: "10px" }}
+          >
+            Reset Viewport
+          </button>
+        </div>
       </div>
-      <div
-        className="border-4 shadow-2xl"
-        ref={dicomImageElement}
-        style={{ width: "512px", height: "512px", background: "black" }}
-      ></div>
     </div>
   );
 }
